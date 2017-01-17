9ff6b7b4c367720ef2cb1f535219ce0dac5313cb
Roman Elizarov <elizarov@gmail.com>
2017-01-18 01:35:35 +0300

    futureAsync{} builder as a shortcut to future(CommonPool)

--- src/kotlinx/coroutines/experimental/future/future.kt
+++ src/kotlinx/coroutines/experimental/future/future.kt
@@ -8,7 +8,10 @@ import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.coroutines.startCoroutine
 
 /**
- * Starts coroutine and returns its results an an implementation of [CompletableFuture].
+ * Starts coroutine in the current coroutine context and returns its results an an implementation of [CompletableFuture].
+ * If is already in the appropriate thread of the context (like in UI thread or in the appropriate thread pool),
+ * then the [block] is executed _in this thread_ until its first suspension point.
+ *
  * The running coroutine is cancelled when the resulting future is cancelled.
  * The [context] that is optionally specified as parameter is added to the context of the parent running coroutine (if any)
  * inside which this function is invoked. The lifetime of the resulting coroutine is subordinate to the lifetime
@@ -22,6 +25,18 @@ public fun <T> future(context: CoroutineContext = EmptyCoroutineContext, block: 
     block.startCoroutine(future)
     return future
 }
+
+/**
+ * Starts coroutine in the [CommonPool] context and returns its results an an implementation of [CompletableFuture].
+ * The [block] is scheduled for execution in the background thread and this function immediately returns to the caller,
+ * similarly to [CompletableFuture.supplyAsync], but with suspending block of code.
+ *
+ * The running coroutine is cancelled when the resulting future is cancelled.
+ * The [context] that is optionally specified as parameter is added to the context of the parent running coroutine (if any)
+ * inside which this function is invoked. The lifetime of the resulting coroutine is subordinate to the lifetime
+ * of the parent coroutine (if any).
+ */
+public fun <T> futureAsync(block: suspend () -> T): CompletableFuture<T> = future(CommonPool, block)
 
 /**
  * Awaits for completion of the future without blocking a thread. This suspending function is cancellable.
