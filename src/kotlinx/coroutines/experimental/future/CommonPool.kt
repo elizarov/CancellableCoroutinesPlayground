efe524018c52651b831bf06e0c06e8c7e1d44ae0
Roman Elizarov <elizarov@gmail.com>
2017-01-17 23:13:02 +0300

    doc fix

--- src/kotlinx/coroutines/experimental/future/CommonPool.kt
+++ src/kotlinx/coroutines/experimental/future/CommonPool.kt
@@ -6,7 +6,7 @@ import java.util.concurrent.ForkJoinPool
 /**
  * Represents [ForkJoinPool.commonPool] as coroutine dispatcher for compute-intensive tasks.
  * [ForkJoinPool] implements efficient work-stealing algorithm for its queues, so every
- * coroutine resumption is dispatched as a separate task even when it is executes inside the pool.
+ * coroutine resumption is dispatched as a separate task even when it already executes inside the pool.
  */
 object CommonPool : CoroutineDispatcher() {
     override fun isDispatchNeeded(): Boolean = true
